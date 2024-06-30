8fe9bedba93d619c6109ea7cef710424d282ab30
Desmend Jetton <desmendjetton@gmail.com>
2024-06-29 22:09:29 -0500

    declaring isPasssword allowed function

--- server/src/utils/__tests__/auth.todo.js
+++ server/src/utils/__tests__/auth.todo.js
@@ -1,5 +1,16 @@
+import {omit} from 'lodash'
+
+function userToJSON(user) {
+  return omit(user, ['exp', 'iat', 'hash', 'salt'])
+}
+
+function isPasswordAllowed(password) {
+  return password.length > 6 && /\d/.test(password) && /\D/.test(password)
+}
+
 test('isPasswordAllowed only allows some passwords', () => {
   // here's where I'll demo things for you :)
+  console.log(isPasswordAllowed)
 })
 
 test('userToJSON excludes secure properties', () => {
@@ -9,19 +20,23 @@ test('userToJSON excludes secure properties', () => {
   // doesn't have any of the properties it's not
   // supposed to.
   // Here's an example of a user object:
-  // const user = {
-  //   id: 'some-id',
-  //   username: 'sarah',
-  //   // ↑ above are properties which should
-  //   // be present in the returned object
-  //
+  const mockUser = {
+    id: 'some-id',
+    username: 'sarah',
+  }
+    // ↑ above are properties which should
+    // be present in the returned object
+  
   //   // ↓ below are properties which shouldn't
   //   // be present in the returned object
-  //   exp: new Date(),
-  //   iat: new Date(),
-  //   hash: 'some really long string',
-  //   salt: 'some shorter string',
-  // }
+    const unSafeUser = {
+    ...mockUser,
+    exp: new Date(),
+    iat: new Date(),
+    hash: 'some really long string',
+    salt: 'some shorter string',
+  }
+  expect(userToJSON(unSafeUser)).toEqual(mockUser)
 })
 
 //////// Elaboration & Feedback /////////
